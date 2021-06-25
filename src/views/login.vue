e3c998f435072a3158aead99e14fcfe754502e3f
dubing <dubing@beyondcent.com>
2021-06-25 16:20:29 +0800

    vue-project2021-05-25

---
+++ src/views/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login_container">
+      login
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+}
+</style>
